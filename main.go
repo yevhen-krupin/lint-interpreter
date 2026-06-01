3bde6201871c3a5ffd5be5431a37c6b080bf7548
Yevhen Krupin <krupineg@gmail.com>
2026-06-01 01:29:52 +0100

    basic value interpretation for the nodes, basic evaluation of + and *

--- main.go
+++ main.go
@@ -2,38 +2,115 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+)
+
+type ResultType string
+
+const (
+	Boolean ResultType = "Boolean"
+	String  ResultType = "String"
+	Int     ResultType = "Int"
+	Unknown ResultType = "Unknown"
+)
+
+type Kind string
+
+const (
+	Expression Kind = "Expression"
+	Atom       Kind = "Atom"
 )
 
 type Node struct {
-	kind  string
-	value string
-	nodes []*Node
+	Kind  Kind
+	Type  ResultType
+	Value []byte
+	Nodes []*Node
 }
 
 func main() {
 	array := []string{
 		"()",
 		"1",
+		"32",
+		"255",
+		"256",
+		"65536",
+		"65537",
+		"16777216",
+		"16777217",
 		"t",
 		"\"Hello, Coding Challenges\"",
 		":CC",
 		"(+ 1 2)",
+		"(* 1 2)",
+		"(* 5 3)",
+		"(defun doublen (n) (* n 2))",
 		"(defun hello() \"Hello Coding Challenges\")",
 		"(format t \"Hello, Coding Challenge World World\")",
+		"(defun fib (n)" +
+			"  (if (< n 2)" +
+			"      n" +
+			"      (+ (fib (- n 1))" +
+			"      (fib (- n 2)))))",
 	}
 
 	for index, element := range array {
 		_, node, _ := atom_node(element, 0)
-		fmt.Printf("\nExpression %d", index)
-		print(node, 0)
+		fmt.Printf("\nExpression %d: `%v`", index, element)
+		t, b, i, s := evaluate(node)
+		fmt.Printf("\nEvaluated as %v %t %d `%v`", t, b, i, s)
+		print(node, "")
+		fmt.Printf("\n---------------")
 	}
 }
 
-func print(node *Node, indent int) {
-	fmt.Printf("\n- %v: %v", node.kind, node.value)
-	for _, n := range node.nodes {
-		print(n, indent+2)
+func print(node *Node, indent string) {
+	value := ""
+	if node.Type == Int {
+		value = strconv.Itoa(bytes_to_int32(node.Value))
 	}
+	if node.Type == String || node.Type == Unknown {
+		value = string(node.Value)
+	}
+	fmt.Printf("\n%v- %v: %v: %v [%v]", indent, node.Kind, node.Type, node.Value, value)
+	for _, n := range node.Nodes {
+		print(n, indent+"  ")
+	}
+}
+
+func evaluate(node *Node) (ResultType, bool, int, string) {
+	if node.Type == Int {
+		return Int, false, bytes_to_int32(node.Value), ""
+	}
+	if node.Type == String {
+		return String, false, 0, string(node.Value)
+	}
+	if node.Type == Boolean {
+		return Boolean, node.Value[0] == 1, 0, ""
+	}
+	if node.Kind == Expression && len(node.Nodes) > 0 {
+		if node.Nodes[0].Kind == Atom && node.Nodes[0].Value[0] == '+' {
+			i := 1
+			r := 0
+			for i < len(node.Nodes) {
+				r += bytes_to_int32(node.Nodes[i].Value)
+				i += 1
+			}
+			return Int, false, r, ""
+		}
+
+		if node.Nodes[0].Kind == Atom && node.Nodes[0].Value[0] == '*' {
+			i := 1
+			r := 1
+			for i < len(node.Nodes) {
+				r *= bytes_to_int32(node.Nodes[i].Value)
+				i += 1
+			}
+			return Int, false, r, ""
+		}
+	}
+	return Unknown, false, 0, ""
 }
 
 func expression_node(input string, pos int) (bool, *Node, int) {
@@ -48,7 +125,7 @@ func expression_node(input string, pos int) (bool, *Node, int) {
 				nodes = append(nodes, node)
 			}
 		}
-		return true, node("expression", "", nodes), pos + 1
+		return true, node(Expression, []byte{}, Unknown, nodes), pos + 1
 	}
 	return false, nil, pos
 }
@@ -63,12 +140,29 @@ func string_node(input string, pos int) (bool, *Node, int) {
 		}
 		// include closing quote
 		pos += 1
-		return true, node("string", input[begin:pos], []*Node{}), pos + 1
+		return true, node(Atom, []byte(input[begin:pos]), String, []*Node{}), pos + 1
 	}
 	return false, nil, pos
 }
 
-// "(defun hello() \"Hello Coding Challenges\")",
+func int_node(input string) (bool, *Node) {
+	i, err := strconv.Atoi(input)
+	if err == nil {
+		return true, node(Atom, *int32_to_bytes(i), Int, []*Node{})
+	}
+	return false, nil
+}
+
+func bool_node(input string) (bool, *Node) {
+	if input == "t" || input == "T" {
+		return true, node(Atom, []byte{1}, Boolean, []*Node{})
+	}
+	if input == "nil" {
+		return true, node(Atom, []byte{0}, Boolean, []*Node{})
+	}
+	return false, nil
+}
+
 func atom_node(input string, pos int) (bool, *Node, int) {
 	// atoms are split by spaces
 	for pos < len(input) && input[pos] == ' ' {
@@ -92,13 +186,38 @@ func atom_node(input string, pos int) (bool, *Node, int) {
 	for pos < len(input) && input[pos] != ' ' && input[pos] != ')' && input[pos] != '(' {
 		pos += 1
 	}
-	return true, node("atom", input[begin:pos], []*Node{}), pos //keep separator in the buffer
+
+	val := input[begin:pos]
+	r, n = int_node(val)
+	if r {
+		return true, n, pos
+	}
+	r, n = bool_node(val)
+	if r {
+		return true, n, pos
+	}
+	return true, node(Atom, []byte(input[begin:pos]), Unknown, []*Node{}), pos //keep separator in the buffer
 }
 
-func node(kind string, value string, nodes []*Node) *Node {
+func node(kind Kind, value []byte, rt ResultType, nodes []*Node) *Node {
 	var node Node
-	node.kind = kind
-	node.value = value
-	node.nodes = nodes
+	node.Type = rt
+	node.Kind = kind
+	node.Value = value
+	node.Nodes = nodes
 	return &node
+}
+
+func int32_to_bytes(i int) *[]byte {
+	buf := make([]byte, 4)
+	buf[3] = byte(i & 0xFF)
+	buf[2] = byte((i & 0xFF00) >> 8)
+	buf[1] = byte((i & 0xFF0000) >> 16)
+	buf[0] = byte((i & 0xFF000000) >> 24)
+	return &buf
+}
+
+func bytes_to_int32(bytes []byte) int {
+	v := int(bytes[0])<<24 | int(bytes[1])<<16 | int(bytes[2])<<8 | int(bytes[3])
+	return v //bytes[0]*256*256*256 + bytes[1]*256*256 | bytes[2]*256 | bytes[3]
 }
